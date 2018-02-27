584f92051bd0b22772fc2cbdd8920caa5ab965f9
Maritia Pangaribuan <maritia.pangaribuan@gmail.com>
2018-02-27 18:17:40 +0700

    FE

--- GVM-Task/public_html/task.js
+++ GVM-Task/public_html/task.js
@@ -1,16 +1,29 @@
-$(function () {
-    $(document).ready(function () {
-        getInstagramFeed('womantalk_com');
-    });
-    var getInstagramFeed = function (name) {
-        $.get('https://www.instagram.com/' + name + '/?__a=1', function (data) {
-            var appendText = ''
-            for (var i = 0; i < 10; i++) {
-                appendText += '<a href="//instagram.com/p/' + data.user.media.nodes[i].code + '" target="_blank">'
-                appendText += '<img src="' + data.user.media.nodes[i].thumbnail_resources[0].src + '" />'
-                appendText += '</a>'
+document.addEventListener('DOMContentLoaded', function () {
+    var elm = document.getElementById('scrollContent');
+    elm.addEventListener('scroll', callFuntion);
+    var array = ['<div class="col-lg-3" style="background-color: red;height: 200px">5</div><div class="col-lg-2" style="background-color: royalblue;height: 200px">5</div><div class="col-lg-2" style="background-color: #23527c;height: 200px">5</div><div class="col-lg-2" style="background-color: window;height: 200px">5</div><div class="col-lg-3" style="background-color: tomato;height: 200px">5</div>', '<div class="col-lg-4" style="background-color: bisque;height: 200px">3</div><div class="col-lg-4" style="background-color: #204d74;height: 200px">3</div><div class="col-lg-4" style="background-color: #faf2cc;height: 200px">3</div>', '<div class="col-lg-6" style="background-color: white;height: 200px">2</div><div class="col-lg-6" style="background-color: blueviolet;height: 200px">2</div>'];
+    var arrayBefore;
+
+    function callFuntion() {
+        var scrollHeight = elm.scrollHeight;
+        var scrollTop = elm.scrollTop;
+        var clientHeight = elm.clientHeight;
+        arrayBefore = array;
+        if (scrollHeight >= clientHeight) {
+            array = shuffleArray(array);
+            while (arrayBefore[0] == array[2]) {
+                array = shuffleArray(array);
             }
-            $('#instafeed').html(appendText)
-        });
+            elm.innerHTML += array[0] + array[1] + array[2];
+        }
+    }
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
     }
 });
